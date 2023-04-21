1447cf7389fe799a5daf93e11b740b1a06985278
bjausseran <benoit.jausseran@ynov.com>
2023-04-21 17:35:34 +0200

    captor datacheck unit test : ok

--- Local_service/Local_app/src/redis/redis_client.js
+++ Local_service/Local_app/src/redis/redis_client.js
@@ -7,12 +7,33 @@ const redis = require('redis');
 // NOTE - REDIS
 const client = redis.createClient({
   url: 'redis://db_local',
-  port: 6379
+  port: 6379,
+  socket: {
+    reconnectStrategy() {
+        console.log('reconnectStrategy', new Date().toJSON());
+        return process.env.JEST_WORKER_ID ? 2147483647 : 5000;
+    }
+}
+});
+
+let isReady = false;
+
+// handle Redis connection errors
+client.on('error', function (err) {
+  isReady = false;
+  console.log('Redis error: ' + err);
+});
+// handle Redis connection errors
+client.on('ready', function () {
+  isReady = true;
+  console.log('Redis ready');
 });
 
 async function redis_connection() {
+  //check if its a test (ye I know)
+  if(process.env.JEST_WORKER_ID) return;
   client.connect(function(err) {
-    if(err) throw err;
+    if(err)  console.log("Redis database not connected : " + err);
     console.log("Redis database connected!")
   })
 }
@@ -27,6 +48,7 @@ function getTimestamp() {
 }
 
 async function postData(data){
+  if(!isReady) return;
     let time = getTimestamp();
     
     console.log("Try post value at " + time);
@@ -38,7 +60,7 @@ module.exports = {
     {
         let data = {
             'captor_id' : captorid,
-            'client_id' : process.env.CLIENT_ID,
+            'client_id' : "q8sf651-654sdf-45s-7qd54",
             'value': value
         };
         // Send to redis DB
@@ -48,23 +70,19 @@ module.exports = {
         let json = JSON.parse(data_send);
         if(json.value && json.captor_id && json.client_id) {
             console.log("value type is OK");
-            postData(data_send);
+            
+            try{
+              postData(data_send);
+            }
+            catch{
+              console.log("Redis database not connected");
+            }
+            return true;
         } else {
             console.log("ERROR value type in database");
+            return false;
         }
         
         
     }
 }
-
-function sum(a, b) {
-  return a + b;
-}
-
-function min(a, b) {
-  return a - b;
-}
-module.exports = {
-  sum,
-  min
-};
---
+++ Local_service/Local_app/tests/unit/datacheck.test.js
@@ -0,0 +1,57 @@
+const redis = require("./../../src/redis/redis_client");
+
+
+describe("Testing redis.PostCaptorData", () => {
+  let result = false;
+  let data = ['0001', '23'];
+
+  describe("Normal creation", () => {
+    beforeAll(() => {
+      result = redis.postCaptorValue(data[0], data[1]);
+    })
+    test("Data should be valid", () => {
+      expect(result).toBe(true);
+    })
+  })
+  
+  describe("no ID creation", () => {
+    beforeAll(() => {
+      data = ['', '23'];
+      result = redis.postCaptorValue(data[0], data[1]);
+    })
+    test("Data shouldn't be valid", () => {
+      expect(result).toBe(false);
+    })
+  })
+  
+  describe("no value creation", () => {
+    beforeAll(() => {
+      data = ['001', ''];
+      result = redis.postCaptorValue(data[0], data[1]);
+    })
+    test("Data shouldn't be valid", () => {
+      expect(result).toBe(false);
+    })
+  })
+  
+  describe("ID is a integer creation", () => {
+    beforeAll(() => {
+      data = [1, '23'];
+      result = redis.postCaptorValue(data[0], data[1]);
+    })
+    test("Data should be valid", () => {
+      expect(result).toBe(true);
+    })
+  })
+  
+  
+  describe("Value is a integer creation", () => {
+    beforeAll(() => {
+      data = ['001', 23];
+      result = redis.postCaptorValue(data[0], data[1]);
+    })
+    test("Data should be valid", () => {
+      expect(result).toBe(true);
+    })
+  })
+})
