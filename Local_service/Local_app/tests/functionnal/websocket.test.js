9ae835b4f518136ca5d6a0d651bad95f3cd11065
bjausseran <benoit.jausseran@ynov.com>
2023-04-28 00:13:02 +0200

    test local workflow

--- Local_service/Local_app/tests/functionnal/websocket.test.js
+++ Local_service/Local_app/tests/functionnal/websocket.test.js
@@ -12,18 +12,20 @@ describe('WebSocket Connection', () => {
         console.log('Response for message is : ' + response);
         expect(response).toBe('OK');
         done();
+        ws.close();
       }
     });
 
     ws.on('open', async () => {
       ws.send(`tocloud//captor_values//{"captor_id": "0001", "value": "23", "created_at": "${new Date().getTime()}"}//insert`)
-      await new Promise((r) => setTimeout(r, 5000));
-      ws.close();
     });
 
 
     ws.on('error', error => {
       done.fail(error);
     });
+
+    await new Promise((r) => setTimeout(r, 5000));
+    ws.close();
   });
 });
