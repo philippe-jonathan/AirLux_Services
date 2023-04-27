96634f913cb56aca34015afc1b34c33cb7eaf508
bjausseran <benoit.jausseran@ynov.com>
2023-04-28 00:00:18 +0200

    test local workflow

--- Local_service/Local_app/tests/functionnal/websocket.test.js
+++ Local_service/Local_app/tests/functionnal/websocket.test.js
@@ -4,21 +4,19 @@ describe('WebSocket Connection', () => {
   test('should connect to WebSocket server', done => {
       const ws = new WebSocket('ws://cloud_app:6001');
 
+      
+    ws.on('message', message => {
+      let response = message.toString('utf8');
+
+      if(response !== 'Welcome to the server!'){
+        console.log('Response for message is : ' + response);
+        expect(response).toBe('OK');
+      }
+    });
+
     ws.on('open', () => {
       done();
-
-      ws.on('message', message => {
-        let response = message.toString('utf8');
-  
-        if(response !== 'Welcome to the server!'){
-          console.log('Response for message is : ' + response);
-          expect(response).toBe('OK');
-        }
-      });
-
       ws.send(`tocloud//captor_values//{"captor_id": "0001", "value": "23", "created_at": "${new Date().getTime()}"}//insert`)
-      
-
       ws.close();
     });
 
