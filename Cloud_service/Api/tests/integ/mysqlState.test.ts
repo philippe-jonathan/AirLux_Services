00b1a3433cbc8628d3b683a3807db4e8ad849079
philippe-jonathan <39793063+philippe-jonathan@users.noreply.github.com>
2023-04-28 10:20:34 +0200

    Update mysqlState.test.ts

--- Cloud_service/Api/tests/integ/mysqlState.test.ts
+++ Cloud_service/Api/tests/integ/mysqlState.test.ts
@@ -1,14 +1,13 @@
 import mysql  from "mysql2";
 
 const pool =  mysql.createPool({
-    host: 'dbcloud',
+    host: 'mysql://dbcloud',
     user: 'root',
     password: 'password',
     database: 'AirLuxDB',
     connectionLimit: 10,
   });
   
-
 pool.getConnection(function(err, connection) {
     if (err) throw err; // not connected!
 })
