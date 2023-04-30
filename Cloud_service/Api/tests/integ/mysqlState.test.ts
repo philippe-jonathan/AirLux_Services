9408dfd2eecfdede999b4d037ef668b622d5898d
philippe-jonathan <39793063+philippe-jonathan@users.noreply.github.com>
2023-04-30 17:20:22 +0200

    Test MySQL Connection

--- Cloud_service/Api/tests/integ/mysqlState.test.ts
+++ Cloud_service/Api/tests/integ/mysqlState.test.ts
@@ -1,23 +1,15 @@
-import mysql  from "mysql2";
-import { describe, expect, beforeAll, afterAll, test } from '@jest/globals';
+import mysql from 'mysql2/promise';
+import { describe, test } from '@jest/globals';
 
-describe('Testing MySQL database connection', () => {
-    let connection : mysql.Connection;
-    beforeAll( async () => {
-        connection = await mysql.createConnection({
-          host: 'db_cloud',
-          user: 'root',
-          password: 'password',
-          database: 'AirLuxDB',
-          charset: 'utf8mb4',
-        });
+describe('Test MySQL Connection', () => {
+  test('Should connect to MySQL', async () => {
+    const connection = await mysql.createConnection({
+      host: 'db_cloud',
+      user: 'root',
+      password: 'password',
+      database: 'AirLuxDB'
     });
     
-    test('Should be connected', () => {
-      expect(connection).toBeDefined();
-    });
-
-    afterAll(async () => {
-      await connection.end();
-    });
+    await connection.end();
+  });
 });
